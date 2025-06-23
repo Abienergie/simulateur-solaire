8482f2f3ac5d3d3f3642dae712a26f69165b0703
abienergie <contact@abie.fr>
2025-06-23 12:18:53 +0000

    Deploying to gh-pages from @ abienergie/simulateur-solaire@b06a5cbccdc38840c007df178c23e0d10d6a9098 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/simulateur-solaire/sw.js', { scope: '/simulateur-solaire/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
